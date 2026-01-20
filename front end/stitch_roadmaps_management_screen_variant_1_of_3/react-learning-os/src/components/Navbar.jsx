60d726cc5b36539cc62cb30538d7961dc11af81e
DHARSHAN SP <dharsandad@gmail.com>
2026-01-21 00:16:57 +0530

    correction in pages final

--- front end/stitch_roadmaps_management_screen_variant_1_of_3/react-learning-os/src/components/Navbar.jsx
+++ front end/stitch_roadmaps_management_screen_variant_1_of_3/react-learning-os/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ export default function Navbar() {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16 items-center">
                     {/* Logo */}
-                    <Link to="/" className="flex items-center gap-2">
+                    <Link to="/about" className="flex items-center gap-2">
                         <div className="bg-primary/10 p-2 rounded-lg">
                             <span className="material-icons-round text-primary">school</span>
                         </div>
